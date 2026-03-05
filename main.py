c23e41063e54964799ca88240db6af93bcf00745
Layrix <jjacewicz02@gmail.com>
2026-03-06 00:15:33 +0100

    added small feat on vocal removal handler

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import demucs
 
 #uses demucs for now, will create a custom model later on
-class vocalRemovalModel:
+class vocalRemovalModelHandler:
+    def __init__(self):
+        self.model = demucs.pretrained.get_model("demucs")
+
+    
+    def remove_vocals(self, audio_file):
+        """Removes vocals from the given audio file and returns the instrumental version."""
+        audio = demucs.audio.load(audio_file)
+        stems = self.model.separate(audio)
+        return stems["vocals"]
     
 def main():
     print("Hello, World!")
