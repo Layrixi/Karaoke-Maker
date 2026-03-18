f7e0e96ce69e4c65fc8011f5917f7030a5c4f3f4
Layrix <jjacewicz02@gmail.com>
2026-03-18 19:55:42 +0100

    Added livereload for dev purposes

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 
+#debug only, delete later
+from livereload import Server
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,5 +10,13 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # debug=false in prod later, change the port adequatly to the pc
-    app.run(debug=True, port=5000)
+    # debug=false in prod later, change the port adequatly to the pc(if possible)
+    #debug code
+    server = Server(app.wsgi_app)
+    server.watch('templates/**/*.html')
+    server.watch('static/**/*.css')
+    server.watch('static/**/*.js')
+    server.serve(port=5000, debug=True)
+
+    #prod code
+    #app.run(debug=True, port=5000)
