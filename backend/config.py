7c655d88fb3d1e0a75f45aacf15bb2ea5c6b4040
Layrix <jjacewicz02@gmail.com>
2026-03-22 01:38:46 +0100

    helper get audio path function added

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,16 @@
+import pathlib
 import torch
 
 
 
+
+#check if cuda is available
 def check_device():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using device: {device}")
     return device
+
+#returns path to the uploaded audio file
+def get_audio_path(filename):
+    return pathlib.Path(__file__).parent / "uploads" / "audio" / filename
+
